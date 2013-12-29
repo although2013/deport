aafb024a4407377162a0a98bba515758437f726d
gehao <althoughghgh@gmail.com>
2013-12-29 22:44:01 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deport::Application.config.secret_key_base = '5d57bc6fa20de3db187715bc5f7a193a2a22a4a4b027248089b556bfdf78cf359cefcf4589f84d308dbef4cf41f611a7477bc5866cfbfbd01ec9124ab4541398'
